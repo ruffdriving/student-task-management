a8f5370048e33e47ef4d2a54b3040b306d2375bc
lalo <ruffdriving@gmail.com>
2026-01-30 13:49:32 +0530

    [updated]

---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../index.css'
+const Register = () => {
+  return (
+    <>
+      <div className="form-container">
+<h1>this is register</h1>
+      </div>
+    </>
+  );
+};
+
+export default Register;
